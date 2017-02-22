a1fed7b188855203fc7f34ebb19b87f6c6d4d15f
yurakuz <kuznevych06@mail.ru>
2017-02-22 16:05:20 +0200

    some

---
+++ 17_02_2017/src/basic/main.java
@@ -0,0 +1,66 @@
+package basic;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		
+		/*int number[] = {10, 20, 30, 40, 50, 60};
+		
+		int maxValue = number [0];
+		
+		
+		
+		for (int i = 0; i < number.length; i++) {
+			if (maxValue < number[i]){
+				maxValue = number[i];
+			}
+			
+		}
+			
+		System.out.println(maxValue);*/
+		
+		
+	/*String name = "my name is Oleh";*/
+		
+		/*System.out.println(name.length());
+		System.out.println(name.charAt(5));
+		System.out.println(name.toUpperCase());
+		System.out.println(name.substring(3, 7));*/
+		
+		
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		int a = 1;
+		int b = 2;
+		int v = 3;
+		int c = 4;
+		int d = 5;
+		int w = 6;
+		System.out.println("Enter ");
+		
+		String input = scanner.next();
+		 
+		
+		
+	
+		
+		 
+		
+		
+		}
+		
+		
+		
+		
+		
+		
+		
+		}
+
+	
+
+
